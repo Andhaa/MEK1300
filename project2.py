651d0ede0b4826e669f5a6442edbc565f6086e38
Andhaa <anders.rudland.haave@gmail.com>
2024-10-17 14:42:58 +0200

    Update project2.py

--- project2.py
+++ project2.py
@@ -1,1 +1,25 @@
+"""
+Oppgaven går ut på å lage et spørreskjema med flervalgspørsmål.
+Den stiller følgende krav: 
 
+Det må være en innloggingsfunksjon for å få tilgang til spørreskjemaet.
+Brukernavn skal være "MEK1300" og passord skal være "Python"
+Dette skal lagres i et dictionary og sendes til en funksjon kalt login_info().
+
+Ved riktig forsøk får man tilgang til selve spørreskjemaet. 
+Hvis ikke skal brukeren så beskjed om feil brukernavn og/eller passord. 
+Brukeren får muligheten til å taste brukernavn og passord på nytt frem til  vellykket forsøk.
+
+Deretter starter flervalgstesten på 10 spørsmål som er vedlagt oppgaven. 
+
+Quizzen avsluttes med at man får vite antall riktige og feil svar. 
+For hvert feil svar skal programmet vise brukerens svar og det riktige svaret.
+
+Hint: Bruk lister for å lagre spørsmål og svar.
+Hint2: Bruk funksjoner for å holde oversikt i koden.
+"""
+
+
+
+def login_info():
+  return 
