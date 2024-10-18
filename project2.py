794cb7ed9f5906e4668adf6203605584df13d916
Anders R Haave <anders.rudland.haave@gmail.com>
2024-10-18 14:20:04 +0200

    formatert lister og laget funksjon for å loope svar og svaralternativ

--- project2.py
+++ project2.py
@@ -42,4 +42,46 @@ def quiz(): #Edward og Anders
   # når det ikke lengre er flere spørsmål 
   # sjekk hva som er riktig og ikke
   # For hvert spørsmål som er svart FEIL, print dette, sammen med riktig svar. 
+  #TODO: (x) formater lister og lag en loop for spørsmål og svaralternativ.
+  #TODO: () en funksjon for å lage tilfeldig rekkefølge på svaralternativene for hvert spørsmål? 
+ 
+  #Liste over alle spørsmål: 
+  questions = ["What is the capital of Norway?", 
+              "What is the currency of Norway?", 
+              "What is the largest city in Norway?",
+              "When is constitution day (the national day) of Norway?",
+              "What color is the background of the Norwegian flag?",
+              "How many countries does Norway border?",	 
+              "What is the name of the university in Trondheim?",	 
+              "How long is the border between Norway and Russia?",	 
+              "Where in Norway is Stavanger?",	 
+              "From which Norwegian city did the world’s famous composer Edvard Grieg come?"]
+  
+  # Dette er en ordnet liste over svaralternativer i grupper på 4 for alle spørsmål. Hvert første svaralternativ i hver gruppe er riktig svar.
+  # dvs, for questions[0] = choises[0:3] gyldige alternativer og choises[0] er riktig svar. 
+  # Vi kan bruke randint til å lage en tilfeldig rekkefølge av svaralternativer senere
+  choises =  ['Oslo','Bergen','Stavanger','Trondheim','Krone','Euro','Pound','DeutscheMark','Oslo','Stavanger','Bergen',
+              'Trondheim','17thMay','27thMay','17thApril','27thApril','Red','White','Blue','Yellow','3','1','2','4',
+              'NTNU','UiS','UiO','NMBU','196km','96km','296km','396km','South-west','North','South','South-east',
+              'Bergen','Oslo','Stavanger','Tromsø']
+  #Liste over bokstaver fra A-D. Brukes til å formatere output
+  letters=[" A:", " B:", " C:"," D:"]
+  #Liste over hva brukeren har svart.
+  user_answers = [] 
+  
+
+  
+  #loop gjennom alle spørsmål og svaralternativer:
+  #TODO: Lag en funksjon for å gjøre slik at svaralternativene kommer i en tilfeldig rekkefølge?  
+  start_pos = 0
+  for i in range(len(questions)):
+    print("Q%i:"%i , questions[i])
+    teller = 0 
+    for t in range(start_pos, start_pos+4):
+      print (letters[teller], choises[t])
+      teller = teller+1
+    start_pos = start_pos + 4
   return
+
+
+quiz()
