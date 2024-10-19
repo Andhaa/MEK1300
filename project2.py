5daea86a0598010caeb2aba97a0428595c06c6fc
Anders R Haave <anders.rudland.haave@gmail.com>
2024-10-19 10:02:03 +0200

    NY FUNKSJON

--- project2.py
+++ project2.py
@@ -63,13 +63,14 @@ def quiz(): #Edward og Anders
   # Dette er en ordnet liste over svaralternativer i grupper på 4 for alle spørsmål. Hvert første svaralternativ i hver gruppe er riktig svar.
   # dvs, for questions[0] = choises[0:3] gyldige alternativer og choises[0] er riktig svar. 
   # Vi kan bruke randint til å lage en tilfeldig rekkefølge av svaralternativer senere
-  choises =  ['Oslo','Bergen','Stavanger','Trondheim','Krone','Euro','Pound','DeutscheMark','Oslo','Stavanger','Bergen',
-              'Trondheim','17thMay','27thMay','17thApril','27thApril','Red','White','Blue','Yellow','3','1','2','4',
+  choises =  ['Oslo','Bergen','Stavanger','Trondheim','Krone','Euro','Pound','Deutsche Mark','Oslo','Stavanger','Bergen',
+              'Trondheim','17th May','27th May','17th April','27th April','Red','White','Blue','Yellow','3','1','2','4',
               'NTNU','UiS','UiO','NMBU','196km','96km','296km','396km','South-west','North','South','South-east',
               'Bergen','Oslo','Stavanger','Tromsø']
   #Liste over bokstaver fra A-D. Brukes til å formatere output
-  letters=[" A:", " B:", " C:"," D:"]
+  letters=["A","B","C","D"]
   #Liste over hva brukeren har svart.
+  global user_answers
   user_answers = [] 
   
 
@@ -78,13 +79,31 @@ def quiz(): #Edward og Anders
   #TODO: Lag en funksjon for å gjøre slik at svaralternativene kommer i en tilfeldig rekkefølge?  
   start_pos = 0
   for i in range(len(questions)):
-    print("Q%i:"%i , questions[i])                  #Ser slik ut: "Q8: Where in Norway is Stavanger?"
+    print("Q%i:"%(i+1) , questions[i])                  #Ser slik ut: "Q8: Where in Norway is Stavanger?"
     teller = 0                                      #For å kunne loope gjennom listen av bokstaver laget jeg en ny teller her, denne vil gå fra 0-3, i loopen under
     for t in range(start_pos, start_pos+4):         #Her looper vi gjennom listen av svaralternativer. vi starter på 0 og slutter på 0+4
-      print (letters[teller], choises[t])
+      print(" "*4,letters[teller],": ", choises[t], sep="") 
       teller = teller+1
     start_pos = start_pos + 4                       #Gjør klar for neste iterasjon av svaralternativer ved å forskyve variablen med 4
+    user_answers.append(user_input())
+  
   return
 
 
+def user_input():
+  ''' Funksjonen kjører så lenge svaret ikke er A, B, C eller D. Returnerer valgt bokstav'''
+  svar = input("Answer:")
+  while svar.capitalize() not in ["A", "B", "C", "D"]:
+    print("Please input your response with a character A, B, C or D")
+    svar = input("Answer:")
+  return svar.capitalize()
+
+def oppsummering(svar, choises):
+  for e in svar: #for hvert element i listen svar
+    if e != in choises[0:4]
+      print("Wrong anwser on question xx. The correct answer is: ")
+  return
+  
+
 quiz()
+print(user_answers)
